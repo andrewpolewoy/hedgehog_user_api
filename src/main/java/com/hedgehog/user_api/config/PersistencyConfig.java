8ec57403ae2725a7187620e4dcb0b875ebbb0cd7
andrewpolewoy <andrewpolewoy@gmail.com>
2022-02-23 19:25:07 +0300

    user-api v1.5 try connect heroku

--- src/main/java/com/hedgehog/user_api/config/PersistencyConfig.java
+++ src/main/java/com/hedgehog/user_api/config/PersistencyConfig.java
@@ -31,9 +31,9 @@ public class PersistencyConfig {
         factory.setDataSource(dataSource);
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
-        properties.setProperty("hibernate.hbm2ddl","update");
+//        properties.setProperty("hibernate.hbm2ddl","update");
 //        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-//        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
         properties.setProperty("show_sql", "true");
 //        properties.setProperty("hibernate.default_schema", "user_api");
 
